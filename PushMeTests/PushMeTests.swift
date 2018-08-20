2158cb4dace91a737fed08ae1e0508b4c4948321
Diego Espinosa <despinosa@despinosas-macbook-pro.local>
2018-08-20 15:04:04 -0500

    Initial Commit

---
+++ PushMeTests/PushMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  PushMeTests.swift
+//  PushMeTests
+//
+//  Created by Diego Espinosa on 8/20/18.
+//  Copyright © 2018 Diego Espinosa. All rights reserved.
+//
+
+import XCTest
+@testable import PushMe
+
+class PushMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
